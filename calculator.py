6b91dc8344773f7f2887a33a5cf60e173260e4a8
Punyarit Klaphachon <punyarit07@gmail.com>
2025-06-04 22:44:05 +0700

    add calculator

--- calculator.py
+++ calculator.py
@@ -11,4 +11,22 @@ L.pack()
 L = Label(GUI, text="Rus@hotmail GitHub")
 L.pack()
 
+v1 = StringVar()
+E1 = ttk.Entry(GUI, textvariable = v1)
+E1.pack()
+
+v2 =StringVar()
+E2 = ttk.Entry(GUI, textvariable = v2)
+E2.pack()
+
+def cal():
+  c = float(v1.get()) * float(v2.get())
+  r1 = v1.get()
+  r2 = v2.get()
+  text = f'{r1}x{r2}={c}'
+  messagebox.showinfo("Rusult", text)
+
+b1 = ttk.Button(GUI, text="Calculate Now!", command=cal)
+b1.pack()
+
 GUI.mainloop()
