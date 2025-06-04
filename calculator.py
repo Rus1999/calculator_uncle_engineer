70f097c4414e3518b4e5c8ffbe1f5af081212965
Punyarit Klaphachon <punyarit07@gmail.com>
2025-06-04 17:37:53 +0700

    add GUI for calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title("Basic Git & GitHub by Rus")
+GUI.geometry('500x700')
+
+L = Label(GUI, text="Punyarit Llaphachon")
+L.pack()
+
+GUI.mainloop()
